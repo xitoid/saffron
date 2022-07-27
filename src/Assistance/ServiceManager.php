82a8630b156b34014b6a7cccf5fa280701f225d6
xitoid <agungsetiawan.afund@gmail.com>
2022-07-28 04:41:47 +0700

    Some fixes in ServiceManager and Service blueprint

--- src/Assistance/ServiceManager.php
+++ src/Assistance/ServiceManager.php
@@ -17,26 +17,29 @@ class ServiceManager
     }
     return false;
   }
-  final public function run($name) {
-    if(!isset($this->services[$name])) die("Undefined Service $name.");
+  final public function run($name, $arguments = array()) {
+    if(!isset($this->services[$name])) {
+      throw new \Exception("Service $name not exists");
+    }
     $service = $this->services[$name];
     if(is_service($service)) {
-      // send warning, not recomended
-      return $this->runGetReturn($service);
+      // please send warning, not recomended
+      return $this->runGetReturn($service, $arguments);
     } else if(is_closure($service)) {
-      return $service();
+      return $service($arguments);
     } else if(is_string($service)) {
       if(class_exists($service)) {
-        return $this->runGetReturn(new $service());
+        return $this->runGetReturn(new $service(), $arguments);
       }
     }
     // if input service wrong, send error here
     // we don't have error handler now, so
-    // I just put echos here
-    echo "<hr>Error, Service '$service' is broken!<hr>\n";
+    // I just throw exception here
+    throw new \Exception("Error, Service '$name' is broken!");
   }
-  final protected function runGetReturn(Service $service) {
-    $service->run();
+  final protected function runGetReturn(Service $service, $arguments) {
+    $service->injectManager($this);
+    $service->run($arguments);
     return $service->getResponse();
   }
 }
--- src/Blueprint/Service.php
+++ src/Blueprint/Service.php
@@ -4,12 +4,18 @@ namespace Xitoid\Saffron\Blueprint;
 class Service
 {
   protected $response;
+  protected $manager;
   public function run() {}
   final public function getResponse() {
     return $this->response;
   }
+  final public function injectManager($manager) {
+    if (!isset($this->manager)) {
+      $this->manager = $manager;
+    }
+  }
   final protected function setResponse($response = "") {
-    if($response == "") return;
+    if ($response == "") return;
     $this->response = $response;
   }
 }
