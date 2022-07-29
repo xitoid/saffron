ce3dadf6b9676cf7e3854596356257a808f33415
xitoid <agungsetiawan.afund@gmail.com>
2022-07-29 12:31:09 +0700

    add src/Blueprint/RouteRegister.php

---
+++ src/Blueprint/RouteRegister.php
@@ -0,0 +1,23 @@
+<?php
+namespace Xitoid\Saffron\Blueprint;
+
+class RouteRegister
+{
+  protected array $route = [];
+  final public function __construct() {
+    $this->route();
+  }
+  public function route() {}
+  final public function getRoutes() {
+    return $this->route;
+  }
+  final protected function set($path, $controller, $action = "index", array $model = [], $values = [], $redir = null) {
+    $this->route[$path] = [
+      'controller' => $controller,
+      'action' => $action,
+      'models' => $model,
+      'values' => $values,
+      'redir' => $redir
+    ];
+  }
+}
